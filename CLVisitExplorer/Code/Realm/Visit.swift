f933b4196af4e0ac0194b0960686bf4f5d0086be
Ben Chatelain <ben@octop.ad>
2016-06-26 21:21:26 -0600

    Rename visit date fields

--- CLVisitExplorer/Code/Realm/Visit.swift
+++ CLVisitExplorer/Code/Realm/Visit.swift
@@ -8,16 +8,30 @@
 
 import RealmSwift
 
+// MARK: - Storage
 class Visit: Object {
     dynamic var id = 0
     dynamic var latitude: Double = 0
     dynamic var longitude: Double = 0
-    dynamic var enterDate: Date?
-    dynamic var enterDeliveryDate: Date?
-    dynamic var exitDate: Date?
-    dynamic var exitDeliveryDate: Date?
-    dynamic var note: String?
 
+    /// The approximate time at which the user arrived at the specified location.
+    dynamic var arrivalDate: Date?
+
+    /// Time the arrival event was delivered to the app.
+    dynamic var arrivalDeliveryDate: Date?
+
+    /// The approximate time at which the user left the specified location.
+    dynamic var departureDate: Date?
+
+    /// Time the arrival event was delivered to the app.
+    dynamic var departureDeliveryDate: Date?
+
+    /// Optional user-supplied note for the visit.
+    dynamic var note: String?
+}
+
+// MARK - RealmSwift.Object
+extension Visit {
     override static func primaryKey() -> String? {
         return "id"
     }
