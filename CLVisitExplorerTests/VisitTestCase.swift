83816faf37513f967d3c396ce6629bf0e2971d2e
Ben Chatelain <ben@octop.ad>
2016-06-28 16:14:59 -0600

    Add VisitTestCase

---
+++ CLVisitExplorerTests/VisitTestCase.swift
@@ -0,0 +1,27 @@
+//
+//  VisitTestCase.swift
+//  CLVisitExplorer
+//
+//  Created by Ben Chatelain on 6/28/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+@testable import CLVisitExplorer
+import XCTest
+
+class VisitTestCase: XCTestCase {
+    var visit: Visit!
+    override func setUp() {
+        super.setUp()
+        visit = Visit()
+    }
+    
+    override func tearDown() {
+        visit = nil
+        super.tearDown()
+    }
+
+    func testVisitDescription() {
+        XCTAssertEqual(visit.description, "Arrival <0.0000,0.0000> +/-0m ")
+    }
+}
