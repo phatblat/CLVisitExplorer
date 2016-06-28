1f666b9ea6fdbd384f7152e00eb9646d8101f033
Ben Chatelain <ben@octop.ad>
2016-06-27 22:37:55 -0600

    Customize Visit description

--- CLVisitExplorer/Code/Realm/Visit.swift
+++ CLVisitExplorer/Code/Realm/Visit.swift
@@ -31,6 +31,29 @@ class Visit: Object {
 
     /// Optional user-supplied note for the visit.
     dynamic var note: String?
+
+    /// Used to format arrival date and time.
+    private lazy var arrivalDateFormatter: DateFormatter = {
+        //$0.dateStyle = .mediumStyle
+        //$0.timeStyle = .shortStyle
+        $0.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return $0
+    }(DateFormatter())
+
+    /// Used to format departure time.
+    private lazy var departureDateFormatter: DateFormatter = {
+        $0.dateStyle = .noStyle
+        $0.timeStyle = .mediumStyle // 3:30:32 PM
+//        $0.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return $0
+    }(DateFormatter())
+
+    private lazy var durationFormatter: DateComponentsFormatter = {
+//        NSDateComponentsFormatterUnitsStyle
+//        Short	“1hr 10min”
+        $0.unitsStyle = .short
+        return $0
+    }(DateComponentsFormatter())
 }
 
 // MARK - RealmSwift.Object
@@ -42,7 +65,44 @@ extension Visit {
 
 // MARK: - Computed Properties
 extension Visit {
+    /// Packages up latitude and longitude into a convenient struct compatible with `CLLocationCoordinate2D`.
     var coordinate: Coordinate {
         return Coordinate(latitude: latitude, longitude: longitude)
+    }
+
+    /// Classifyes a visit event.
+    ///
+    /// - Arrival:   Visit with only an arrival time.
+    /// - Departure: Visit with a departure time.
+    enum VisitType: String {
+        case Arrival, Departure
+    }
+
+    /// Determines which type of a visit.
+    var type: VisitType {
+        return (departureDate == nil) ? .Arrival : .Departure
+    }
+
+    var duration: DateComponents? {
+        guard let departure = departureDate else { return nil }
+        return Calendar.current().components([.day, .hour, .minute, .second], from: departure)
+    }
+}
+
+// MARK: - CustomStringConvertible
+extension Visit {
+    override var description: String {
+        var time: String = ""
+        if let arrival = arrivalDate {
+            time = arrivalDateFormatter.string(from: arrival)
+        }
+        if let departure = departureDate {
+            time += " - "
+            time += departureDateFormatter.string(from: departure)
+            time += " "
+            time += durationFormatter.string(from: duration!)!
+        }
+
+        return String(format: "%@ <%.4f,%.4f> +/-%.0fm %@", type.rawValue, latitude, longitude, horizontalAccuracy, time)
     }
 }
