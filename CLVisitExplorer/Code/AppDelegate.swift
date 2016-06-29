5edb0ed44c477e659551c1ad001e29f2edb0b47b
Ben Chatelain <ben@octop.ad>
2016-06-29 14:19:32 -0600

    Include badges in notification permission request

--- CLVisitExplorer/Code/AppDelegate.swift
+++ CLVisitExplorer/Code/AppDelegate.swift
@@ -22,7 +22,7 @@ extension AppDelegate: UIApplicationDelegate {
         }
         LocationManager.shared.restartLocationServices()
 
-        let settings = UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
+        let settings = UIUserNotificationSettings(types: [.alert, .sound, .badge], categories: nil)
         application.registerUserNotificationSettings(settings)
         printDisplayName()
         configureRealm()
