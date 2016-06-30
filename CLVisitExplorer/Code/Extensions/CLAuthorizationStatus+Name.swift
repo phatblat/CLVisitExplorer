0bdcdded5d67f79f3cd33d7668c5bb5338db66e9
Ben Chatelain <ben@octop.ad>
2016-06-29 18:26:27 -0600

    Customize CLAuthorizationStatus.description

---
+++ CLVisitExplorer/Code/Extensions/CLAuthorizationStatus+Name.swift
@@ -0,0 +1,22 @@
+//
+//  CLAuthorizationStatus+Name.swift
+//  CLVisitExplorer
+//
+//  Created by Ben Chatelain on 6/29/16.
+//  Copyright © 2016 Ben Chatelain. All rights reserved.
+//
+
+import CoreLocation
+
+// MARK - CustomStringConvertible
+extension CLAuthorizationStatus: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case notDetermined:         return "notDetermined"
+        case restricted:            return "restricted"
+        case denied:                return "denied"
+        case authorizedAlways:      return "authorizedAlways"
+        case authorizedWhenInUse:   return "authorizedWhenInUse"
+        }
+    }
+}
